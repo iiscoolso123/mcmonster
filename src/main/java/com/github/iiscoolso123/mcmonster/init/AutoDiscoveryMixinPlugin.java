1d4d878f1b2c7c565a4e2c08b98768273955e7de
GitHub Action <action@github.com>
2025-04-06 18:36:45 +0000

    Initialize template repository proper
    Set package name to com.github.iiscoolso123.mcmonster
    Set project name to mcmonster
    Set modid to mcmonster

--- src/main/java/com/github/iiscoolso123/mcmonster/init/AutoDiscoveryMixinPlugin.java
+++ src/main/java/com/github/iiscoolso123/mcmonster/init/AutoDiscoveryMixinPlugin.java
@@ -1,4 +1,4 @@
-package com.example.init;
+package com.github.iiscoolso123.mcmonster.init;
 
 import org.spongepowered.asm.lib.tree.ClassNode;
 import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
